1168906b8ffb97ab63c56b192a1c8eab81782f6c
Franklin <writefranklyn@gmail.com>
2024-08-11 13:32:05 +0100

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+
+        if (isDarkMode) {
+            document.body.classList.remove('dark-mode');
+        } else {
+            document.body.classList.add('dark-mode');
+        }
+    };
+
     return (
         <div className="container">
             <div className="logo">
-                <h1><i class="fa-solid fa-helmet-safety fa-flip"></i>StoneWorks</h1>
+                <h1><i className="fa-solid fa-helmet-safety fa-flip"></i> StoneWorks</h1>
             </div>
             <div className="nav-links">
                 <i className="fa-solid fa-bell"></i>
                 <i className="fa-solid fa-user"></i>
+                <i
+                    className={`fa-solid ${isDarkMode ? 'fa-lightbulb' : 'fa-moon'}`}
+                    onClick={toggleDarkMode}
+                ></i>
             </div>
         </div>
     );
